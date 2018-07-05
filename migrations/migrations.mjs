af560b848cf3be19abc8fffb96da39ce12d5af69
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2018-07-06 03:28:58 +0500

    Syntax with sync and await

--- migrations/migrations.mjs
+++ migrations/migrations.mjs
@@ -2,10 +2,14 @@ import { PlatformAggregate } from './PlatformAggregate'
 
 
 
-console.log("Starting Migrations....")
-PlatformAggregate.sync({force: true}).then(() => {
-  console.log("PlatformAggregate Table Created")
-}).catch((err) => {
-	console.log("Some Error Occured", err)
-})
-console.log("Migrations Completed")
+
+sync function runMigrations() {
+
+	console.log("Starting Migrations....")
+	await PlatformAggregate.sync({force: true})
+	console.log("PlatformAggregate Table Created")
+	console.log("Migrations Completed")
+
+}
+
+runMigrations()

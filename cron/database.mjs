ad8f6f70b9fb2e3619013aee481301052a880bdf
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2018-07-06 03:06:04 +0500

    Problem Connecting with database

--- cron/database.mjs
+++ cron/database.mjs
@@ -2,4 +2,11 @@ import  Sequelize  from 'sequelize'
 
 export const sequelize = new Sequelize('mysql://root:kibo4321@localhost:22/kibodash')
 
-
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });

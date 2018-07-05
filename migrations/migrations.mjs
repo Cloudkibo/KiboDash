2672fbf1b06812d336ff0cfb9d3cf66167a720af
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2018-07-05 16:49:52 +0500

    Migrations

--- migrations/migrations.mjs
+++ migrations/migrations.mjs
@@ -1,5 +1,7 @@
 import { PlatformAggregate } from './PlatformAggregate'
 
+console.log("Starting Migrations....")
 PlatformAggregate.sync({force: true}).then(() => {
   console.log("PlatformAggregate Table Created")
 });
+console.log("Migrations Completed")

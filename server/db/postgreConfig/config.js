83e225be528aae40d0fc1d8f6cdef7bb45bc55cc
assadbintahir <asadullah.09471@khi.iba.edu.pk>
2018-09-18 13:02:21 +0500

    db name

--- server/db/postgreConfig/config.js
+++ server/db/postgreConfig/config.js
@@ -23,7 +23,7 @@ module.exports = {
     'dialect': 'mysql'
   },
   'production': {
-    'username': 'asad',
+    'username': process.env.PROD_USERNAME,
     'password': process.env.PROD_PASSWORD,
     'database': process.env.PROD_DATABASE,
     'host': process.env.PROD_HOST,

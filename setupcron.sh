337e43ab8205b7bb70d9922823acde35ab7870e1
assadbintahir <asadullah.09471@khi.iba.edu.pk>
2018-09-18 13:43:25 +0500

    commit -n

--- setupcron.sh
+++ setupcron.sh
@@ -1,4 +1,4 @@
 # Don't you dare to change this.
 # Following pattern (1 0 * * */1) will run the script at 00:00 (12:01 AM) every day of the week
 
-echo "$(echo '* * * * *' DEBUG=$DEBUG URL=$URL NODE_ENV=$NODE_ENV DOMAIN=$DOMAIN $NODE_PATH --trace-warnings $CRON_SCRIPT_PATH >> /root/logfile.txt 2>&1 ; crontab -l)" | crontab -
+echo "$(echo '* * * * *' DEBUG=$DEBUG URL=$URL NODE_ENV=$NODE_ENV DOMAIN=$DOMAIN $NODE_PATH --trace-warnings $CRON_SCRIPT_PATH '>> /root/logfile.txt 2>&1' ; crontab -l)" | crontab -

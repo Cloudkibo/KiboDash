6b9cd4f9803c6eedd9adba0ae5e6b1b863c8e7e6
anishachhatwani <i130315@nu.edu.pk>
2019-03-29 12:33:48 +0500

    fixing issues

--- cron/fetch.js
+++ cron/fetch.js
@@ -70,53 +70,53 @@ const getwordpressauto = '/getWordpressAutoposting'; // Don't remove this semico
     })
 
   // Request for Page Aggregate Data
-  models.PageAggregate.findAll({order: [['createdAt', 'DESC']], limit: 1})
-    .then((data) => {
-      // We are making a string because request library only supports strings for formData
-      let startDate = data[0] &&
-                       data[0].dataValues &&
-                       data[0].dataValues.createdAt &&
-                       data[0].dataValues.createdAt
-      if (startDate === undefined) {
-        startDate = ''
-      }
-
-      let optionsPage = {
-        form: {startDate: startDate},
-        url: baseURL + getpagedata
-      }
-      reqForPage(optionsPage)
-    })
-    .catch((err) => {
-      if (err) {
-        logger.serverLog(TAG, 'Error Page Date fetching:' + JSON.stringify(err))
-      }
-    })
-
-  // Request for Autoposting Aggregate Data
-  models.AutopostingAggregate.findAll({order: [['createdAt', 'DESC']], limit: 1})
-    .then((data) => {
-      // We are making a string because request library only supports strings for formData
-      let startDate = data[0] &&
-                       data[0].dataValues &&
-                       data[0].dataValues.createdAt &&
-                       data[0].dataValues.createdAt
-      if (startDate === undefined) {
-        startDate = ''
-      }
-      let optionsAutoposting = {
-        form: {
-          startDate: startDate
-        },
-        url: baseURL
-      }
-      reqForAutoposting(optionsAutoposting)
-    })
-    .catch((err) => {
-      if (err) {
-        logger.serverLog(TAG, 'Error Autoposting Date fetching:' + JSON.stringify(err))
-      }
-    })
+  // models.PageAggregate.findAll({order: [['createdAt', 'DESC']], limit: 1})
+  //   .then((data) => {
+  //     // We are making a string because request library only supports strings for formData
+  //     let startDate = data[0] &&
+  //                      data[0].dataValues &&
+  //                      data[0].dataValues.createdAt &&
+  //                      data[0].dataValues.createdAt
+  //     if (startDate === undefined) {
+  //       startDate = ''
+  //     }
+  //
+  //     let optionsPage = {
+  //       form: {startDate: startDate},
+  //       url: baseURL + getpagedata
+  //     }
+  //     reqForPage(optionsPage)
+  //   })
+  //   .catch((err) => {
+  //     if (err) {
+  //       logger.serverLog(TAG, 'Error Page Date fetching:' + JSON.stringify(err))
+  //     }
+  //   })
+  //
+  // // Request for Autoposting Aggregate Data
+  // models.AutopostingAggregate.findAll({order: [['createdAt', 'DESC']], limit: 1})
+  //   .then((data) => {
+  //     // We are making a string because request library only supports strings for formData
+  //     let startDate = data[0] &&
+  //                      data[0].dataValues &&
+  //                      data[0].dataValues.createdAt &&
+  //                      data[0].dataValues.createdAt
+  //     if (startDate === undefined) {
+  //       startDate = ''
+  //     }
+  //     let optionsAutoposting = {
+  //       form: {
+  //         startDate: startDate
+  //       },
+  //       url: baseURL
+  //     }
+  //     reqForAutoposting(optionsAutoposting)
+  //   })
+  //   .catch((err) => {
+  //     if (err) {
+  //       logger.serverLog(TAG, 'Error Autoposting Date fetching:' + JSON.stringify(err))
+  //     }
+  //   })
 }())
 
 const reqForPlatform = function (optionsPlatform) {

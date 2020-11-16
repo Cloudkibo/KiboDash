508a936f77176ae3c6f15774a5a1adafff8f0750
sania <sania@cloudkibo.com>
2020-11-16 14:34:12 +0500

    typo

--- server/db/models/pagesUpdated.js
+++ server/db/models/pagesUpdated.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const PagesUpdated = sequelize.define('PagesUpdated', {
     updateId: DataTypes.INTEGER,
-    updateAt: DataTypes.DATE
+    updatedAt: DataTypes.DATE
   }, {})
   PagesUpdated.associate = function (models) {
     // associations can be defined here

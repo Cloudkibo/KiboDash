1f0a0929cba625717d1e38595b54fa785e34272d
ImranBinShoukat <imran.shoukat1995@gmail.com>
2020-11-26 16:58:42 +0500

    SDApagewise CRUD

---
+++ server/api/v1/slaDashboard/pageWise/controller.js
@@ -0,0 +1,50 @@
+const logger = require('../../../../components/logger')
+const models = require('../../../../db/models')
+
+const TAG = '/server/api/v1/slaDashboard/pageWise/controller.js'
+
+exports.find = function (req, res) {
+  models.SDAPageWise.findAll({where: req.body})
+    .then((data) => {
+      return res.status(200).json({ status: 'success', payload: data })
+    })
+    .catch((err) => {
+      logger.serverLog(TAG, err)
+      return res.status(500).json({ status: 'failed', description: 'Failed to fetch pageWise records' })
+    })
+}
+
+exports.create = function (req, res) {
+  models.SDAPageWise.create(req.body)
+    .then(savedData => {
+      return res.status(200).json({ status: 'success', payload: savedData })
+    })
+    .catch((err) => {
+      logger.serverLog(TAG, err)
+      return res.status(500).json({ status: 'failed', description: 'Failed to create pageWise record' })
+    })
+}
+
+exports.update = function (req, res) {
+  const query = { where: req.body.query }
+  const updated = req.body.updated
+  models.SDAPageWise.update(updated, query)
+    .then(savedData => {
+      res.status(200).json({ status: 'success', payload: savedData })
+    })
+    .catch((err) => {
+      logger.serverLog(TAG, err)
+      res.status(500).json({ status: 'failed', description: 'Failed to update pageWise record' })
+    })
+}
+
+exports.delete = function (req, res) {
+  models.SDAPageWise.destroy({where: req.body})
+    .then(savedData => {
+      res.status(200).json({ status: 'success', payload: savedData })
+    })
+    .catch((err) => {
+      logger.serverLog(TAG, err)
+      res.status(500).json({ status: 'failed', description: 'Failed to delete pageWise record' })
+    })
+}

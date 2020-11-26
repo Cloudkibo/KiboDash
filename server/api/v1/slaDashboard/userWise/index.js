3564f6062955145c6e69d53879ceefc6aef5031a
ImranBinShoukat <imran.shoukat1995@gmail.com>
2020-11-26 18:06:40 +0500

    SDAuserwise CRUD

---
+++ server/api/v1/slaDashboard/userWise/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+const validate = require('express-jsonschema').validate
+const auth = require('../../../auth/auth.service')
+const controller = require('./controller')
+const validationSchema = require('./validationSchema')
+
+router.post('/find',
+  auth.isAuthenticated(),
+  validate({ body: validationSchema.findPayload }),
+  controller.find)
+
+router.post('/',
+  auth.isAuthenticated(),
+  validate({ body: validationSchema.createPayload }),
+  controller.create)
+
+router.put('/',
+  auth.isAuthenticated(),
+  validate({ body: validationSchema.updatePayload }),
+  controller.update)
+
+router.delete('/',
+  auth.isAuthenticated(),
+  validate({ body: validationSchema.findPayload }),
+  controller.delete)
+
+module.exports = router
